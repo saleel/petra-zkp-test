2c313543fb90850e4bb3bc1071f46b8f391913ab
saleel <saleel@saleel.xyz>
2026-03-18 03:22:30 +0400

    chore: fix config

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  outputFileTracingIncludes: {
-    "/api/register": [
-      "./node_modules/@aztec/bb.js/dest/node/**/*",
-      "./node_modules/@aztec/bb.js/dest/node-cjs/**/*",
-    ],
-  },
   serverExternalPackages: ["@aztec/bb.js"],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push({
-        "@aztec/bb.js": "@aztec/bb.js",
-      });
-    }
-
-    // Handle WASM files
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-    };
-
-    return config;
+  outputFileTracingIncludes: {
+    '/api/faucet': ["./node_modules/@aztec/bb.js/dest/node/**/*", "./node_modules/@aztec/bb.js/dest/node-cjs/**/*"],
   },
 };
 
